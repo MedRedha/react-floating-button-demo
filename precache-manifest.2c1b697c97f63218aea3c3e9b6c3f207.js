e77af9fabc48f5ae0af2f204722366a22cc99b5e
na6im <nassim.amokrane@hotmail.com>
2020-04-27 03:02:45 +0100

    Updates

---
+++ precache-manifest.2c1b697c97f63218aea3c3e9b6c3f207.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7179011769551acc0f77d27186e70e5",
+    "url": "/react-floating-button-demo/index.html"
+  },
+  {
+    "revision": "2b5560630410c91a45d5",
+    "url": "/react-floating-button-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c46bd034a24f5299a28a",
+    "url": "/react-floating-button-demo/static/js/2.d761b66c.chunk.js"
+  },
+  {
+    "revision": "5839521b381d6ae8d4739a6544c25d5a",
+    "url": "/react-floating-button-demo/static/js/2.d761b66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5560630410c91a45d5",
+    "url": "/react-floating-button-demo/static/js/main.a7203652.chunk.js"
+  },
+  {
+    "revision": "759a537214d0fe8c3c02",
+    "url": "/react-floating-button-demo/static/js/runtime-main.4d138bdb.js"
+  },
+  {
+    "revision": "7e65f1aef6ba85d69431857d1875dd51",
+    "url": "/react-floating-button-demo/static/media/DOWNLOAD.7e65f1ae.svg"
+  },
+  {
+    "revision": "6f01845ce49b721a9bd08cd295038234",
+    "url": "/react-floating-button-demo/static/media/FORWARD 2.6f01845c.svg"
+  },
+  {
+    "revision": "03d0b9097774513415a1b181538c6fe1",
+    "url": "/react-floating-button-demo/static/media/GIFT.03d0b909.svg"
+  },
+  {
+    "revision": "5a25150d926b51d28e42ea616f30ab19",
+    "url": "/react-floating-button-demo/static/media/GRID.5a25150d.svg"
+  },
+  {
+    "revision": "695748c18d65f5b90c81178ec14c0a6a",
+    "url": "/react-floating-button-demo/static/media/HOME.695748c1.svg"
+  },
+  {
+    "revision": "cf396ce13c20f65f02aa188ed15b585d",
+    "url": "/react-floating-button-demo/static/media/MAP PIN.cf396ce1.svg"
+  },
+  {
+    "revision": "a4eb782b1f07259f7ff68975dc072162",
+    "url": "/react-floating-button-demo/static/media/MAP.a4eb782b.svg"
+  },
+  {
+    "revision": "e7f8db9ce7e4c737685e9a164fc93867",
+    "url": "/react-floating-button-demo/static/media/MUSIC.e7f8db9c.svg"
+  }
+]);
